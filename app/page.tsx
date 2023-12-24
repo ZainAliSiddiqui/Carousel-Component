3c398a8f3de8dadf60106f82e669d91f9af58c83
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-24 15:04:05 +0500

    Added Carousel component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Carousel } from "@components";
+import { imgItem } from "@constants";
+
 export default function App() {
-	return <div>App</div>;
+	return (
+		<div className="w-full items-center justify-center flex h-screen bg-gray-400">
+			<Carousel slides={imgItem} />
+		</div>
+	);
 }
