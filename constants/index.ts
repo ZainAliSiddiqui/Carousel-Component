76f2b58f88c69680a4f3a6a3518c1ae316de4821
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-24 15:03:36 +0500

    Exported constant for Carousel component

---
+++ constants/index.ts
@@ -0,0 +1,32 @@
+import { lock } from "@public";
+
+export const imgItem = [
+    {
+        id: 1,
+        src: lock
+    },
+    {
+        id: 2,
+        src: lock
+    },
+    {
+        id: 3,
+        src: lock
+    },
+    {
+        id: 4,
+        src: lock
+    },
+    {
+        id: 5,
+        src: lock
+    },
+    {
+        id: 6,
+        src: lock
+    },
+    {
+        id: 7,
+        src: lock
+    },
+];
